51ad5d9d457442e4f076c99e7e576ff9b042ca27
ana luiza <99657943+nalu5@users.noreply.github.com>
2022-10-04 17:29:49 -0300

    Add files via upload

---
+++ appClimaAtual/AppClimaAtual/AppClimaAtual/AppClimaAtual/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+using AppClimaAtual.Model;
+using AppClimaAtual.Services;
+
+namespace AppClimaAtual
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.Title = "Previsão do Tempo";
+            this.BindingContext = new Tempo();
+        }
+
+        private async void btnPrevisao_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!String.IsNullOrEmpty(cidadeEntry.Text))
+                {
+                    Tempo previsaoDoTempo = await DataService.GetPrevisaoDoTempo(cidadeEntry.Text);
+                    this.BindingContext = previsaoDoTempo;
+                    btnPrevisao.Text = "Nova Previsâo";
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro", ex.Message, "OK");
+            }
+        }
+    }
+}
